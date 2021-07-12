b968fb49a51c814e6ca08dc91498892e7e38def3
Kelvin Soh <kelvinsoh@pop-os.localdomain>
2021-07-12 21:45:14 +0800

    eslint

---
+++ src/__tests__/consoleTest.test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_1 = require("../index");
+test('My Test', () => {
+    expect(index_1.consoleTest()).toBe(1);
+});
---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.consoleTest = void 0;
+const consoleTest = () => {
+    console.log('hello');
+    return 1;
+};
+exports.consoleTest = consoleTest;
